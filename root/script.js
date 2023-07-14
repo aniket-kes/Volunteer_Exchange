7385c97733f588500c241990f58d3c247c95fe8e
Prathamesh <kapoorprathamesh02@gmail.com>
2023-07-14 20:20:00 +0530

    added number animation and margin to login button

--- root/script.js
+++ root/script.js
@@ -16,3 +16,48 @@ function slider(){
    
 }
 setInterval(slider,2000);
+
+
+
+//animate numbers
+run();
+function run(){
+const speed=200;
+
+const counterNum=document.getElementsByClassName("num");
+
+Array.prototype.forEach.call(counterNum,function (currentValue) {
+    const targetNum= parseInt(currentValue.innerText);
+   
+    var initialNum=0;
+    const incrementNum=(targetNum/speed);
+  
+   function count(){
+   if(initialNum < targetNum)
+    {
+        currentValue.innerText = parseInt(parseInt(initialNum) + parseInt(incrementNum));
+        initialNum=currentValue.innerText;
+       
+    }
+    const myTimeout= setTimeout(count, 20);
+
+    }
+    count();
+    
+/*
+    function timer()
+    {
+        if(i<10)
+        {
+        currentValue.innerText=i;
+        console.log(currentValue.innerText);
+        i++;
+        const myTimeout=setTimeout(timer, 100);
+        }
+       
+    }
+    timer();*/
+}
+);
+
+};
