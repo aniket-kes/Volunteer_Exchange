f379e00c9c29fbaeed7f4d6db1e40844d60c3544
Aniket Kesarwani <aniket.kesarwani@spit.ac.in>
2023-07-25 13:36:32 +0530

    updates

--- root/script.js
+++ root/script.js
@@ -61,21 +61,23 @@ toggleBtn.onclick = function() {
         : 'fa-solid fa-bars'
 }
 
+// $('html,body').scrollTop(0);
+
 //scroll to reveal element
-function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
+// function reveal() {
+//     var reveals = document.querySelectorAll(".reveal");
   
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
+//     for (var i = 0; i < reveals.length; i++) {
+//       var windowHeight = window.innerHeight;
+//       var elementTop = reveals[i].getBoundingClientRect().top;
+//       var elementVisible = 150;
   
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
-    }
-  }
+//       if (elementTop < windowHeight - elementVisible) {
+//         reveals[i].classList.add("active");
+//       } else {
+//         reveals[i].classList.remove("active");
+//       }
+//     }
+//   }
   
-  window.addEventListener("scroll", reveal);
+//   window.addEventListener("scroll", reveal);
